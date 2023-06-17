8b169d762ff1a7d6051a520e1c864a5dcc12b84f
Just2sire <le05onze2004@gmail.com>
2023-06-17 09:57:32 +0000

    Mise ne place de l'authentification avec le frontReact

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,62 +5,41 @@ const User = require('../models/user');
 
 const register = (req, res) => {
     const {username, password} = req.body
-    const exists = User.findOne({username: username})
+    User.findOne({username: username})
     .then((user) => {
         if (user !== null) {
-            return res.json({error: `L'utilisateur ${username} existe déjà !!!`});
+            return res.status(500).json({message: `L'utilisateur ${username} existe déjà !!!`});
         };
-    }).catch(err => { throw Error(err.message) })
+    }).catch(err => { 
+      console.error(err.message);
+      return res.status(500).json({message: `Erreur lors de l'inscription`});
+    })
 
     bcrypt.genSalt(10, (err, salt) => {
-        if (err) throw Error(err.message);
+        if (err) res.status(500).json({message: `Erreur lors de l'inscription`});
         bcrypt.hash(password, salt, (err, hash) => {
-            if (err) throw Error(err.message);
+            if (err) console.error(err.message);
 
-            password = hash;        
+            req.body.password = hash;        
             const user = new User(req.body);
             user.save()
             .then((user) => {
-                // const token = user.generateAuthToken();
-                res.json({success: "Votre compte a bien été crée"});
+                const token = user.generateAuthToken();
+                const userId = user.id;
+                res.json({
+                  success: "Votre compte a bien été crée",
+                  token,
+                  userId
+                });
             })
             .catch((err) => {
-                res.json({error: err.message});
+                res.status(500).json({message: err.message});
                 throw err.message
             })
         })
     });
 }
 
-/*const register = (req, res) => {
-    const exists = User.exists({ name: req.body.name });
-    if (exists) {
-      return res.json({ error: 'User already exists' });
-    }
-  
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) return next(err);
-  
-      bcrypt.hash(req.body.password, salt, (err, hash) => {
-        if (err) return next(err);
-  
-        req.body.password = hash;
-        const user = new User(req.body);
-        user.save()
-          .then(() => {
-            const payload = { id: user.id, username: user.username };
-            const token = jwt.sign(payload, process.env.JWT_SECRET);
-  
-            res.json({ success: 'Your account has been created', token });
-          })
-          .catch((err) => {
-            res.json({ error: err.message });
-            throw new Error("Error during registration");
-          });
-      });
-    });
-  };*/
-  
 const login = (req, res) => {
   const { email, password } = req.body;
     User.findOne({email: email})
@@ -69,21 +48,23 @@ const login = (req, res) => {
       bcrypt.compare(password, user.password)
         .then(match => {
           if (!match) {
-            return res.json({ error: 'Identifiants invalides' });
+            return res.status(500).json({ message: 'Identifiants invalides' });
           }
 
           // Générer le token JWT
           const token = user.generateAuthToken();
+          // console.log(token);
 
           // Renvoyer le token en tant que réponse
           res.json({ 
             success: "Connecté avec succès",
-            token 
+            token,
+            userId: user._id
           });
         })
         .catch(err => {
-          console.error(err);
-          res.json({ error: "Erreur lors de l'authentification" });
+          console.error(err.message);
+          res.status(500).json({ message: "Erreur lors de l'authentification" });
         });
     })
 };
